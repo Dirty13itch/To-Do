7f7e9d47579967bb86bb674f4a6464cb379d4709
Shaunzy <Dirty13itch@users.noreply.github.com>
2026-01-16 17:24:58 -0600

    Make app runnable with Vite + add lucide-react

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+﻿import TodoApp from "./TodoApp.jsx";
+
+export default function App() {
+  return (
+    <div style={{ padding: 24, maxWidth: 900, margin: "0 auto" }}>
+      <TodoApp />
+    </div>
+  );
+}
